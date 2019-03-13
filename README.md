6e1f14f97069bef02d5ed3a5ec51682bb4335dfb
Merry Cimakasky <mkcimakasky@gmail.com>
2019-03-13 09:55:15 -0700

    img-prototype.render

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # lab-02
 Lab 02 for Code Fellows
+Number and name of feature: Feature 1: Display Images
+
+Estimate of time needed to complete: 60 mins
+
+Start time: 9:30
+
+Finish time: _____
+
+Actual time needed to complete: _____
--- js/app.js
+++ js/app.js
@@ -1,3 +1,33 @@
 $(function() {
-  
+  'use strict';
+
+  const $pickTemplate = $('#phto-template');
+
+  function Picture (pic) {
+    this.imageurl = pic.img_url;
+    this.title = pic.title;
+    this.description = pic.description;
+    this.keyword = pic.keyword;
+    this.horns = pic.horns;
+  }
+
+  Picture.allPictures = [];
+
+  Picture.prototype.render = function(){
+    $('main').append('<section class = "clone"></section>');
+
+    let $picClone = $('section[class = "clone"]');
+    let $picHTML = $('#photo-template').html(); 
+
+    $picClone.html($picHTML);
+    $picClone.find('h2').text(this.name);
+    $picClone.find('img').attr('src', this.imageurl);
+    $picClone.find('p').text(this.description);
+    $picClone.removeClass('clone');
+    $picClone.attr('class', this.name);
+  };
+
+
+
 });
+//copy html templat to JS 
