1afad479e779b9069f41e75fb58039101188dca5
Merry Cimakasky <mkcimakasky@gmail.com>
2019-03-13 10:19:05 -0700

    refactored img render function

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ Estimate of time needed to complete: 60 mins
 
 Start time: 9:30
 
-Finish time: _____
+Finish time: 10:18
 
-Actual time needed to complete: _____
+Actual time needed to complete: 48 mins
--- js/app.js
+++ js/app.js
@@ -1,49 +1,53 @@
 $(function() {
   'use strict';
 
-  function Picture (pic) {
-    this.imageurl = pic.image_url;
-    this.title = pic.title;
-    this.description = pic.description;
-    this.keyword = pic.keyword;
-    this.horns = pic.horns;
-  }
+  let _displayImages = () => {
 
-  Picture.allPictures = [];
 
-  Picture.prototype.render = function(){
-    $('main').append('<section class = "clone"></section>');
+    function Picture (pic) {
+      this.imageurl = pic.image_url;
+      this.title = pic.title;
+      this.description = pic.description;
+      this.keyword = pic.keyword;
+      this.horns = pic.horns;
+    }
 
-    let $picClone = $('section[class = "clone"]');
-    let $picHTML = $('#photo-template').html();
+    Picture.allPictures = [];
 
-    
+    Picture.prototype.render = function(){
+      $('main').append('<section class = "clone"></section>');
 
-    $picClone.html($picHTML);
-    $picClone.find('h2').text(this.title);
-    $picClone.find('img').attr('src', this.imageurl);
-    $picClone.find('p').text(this.description);
-    $picClone.removeClass('clone');
-    $picClone.attr('class', this.title + ' animal');
+      let $picClone = $('section[class = "clone"]');
+      let $picHTML = $('#photo-template').html();
+
+
+
+      $picClone.html($picHTML);
+      $picClone.find('h2').text(this.title);
+      $picClone.find('img').attr('src', this.imageurl);
+      $picClone.find('p').text(this.description);
+      $picClone.removeClass('clone');
+      $picClone.attr('class', this.title + ' animal');
+    };
+
+
+
+    Picture.readJSON = () => {
+      $.get('data/page-1.json', 'json')
+        .then(data => {
+          data.forEach(item => {
+            Picture.allPictures.push(new Picture(item));
+          });
+        })
+        .then(Picture.loadPictures);
+    };
+
+    Picture.loadPictures = () => {
+      Picture.allPictures.forEach(pic => pic.render());
+      $('#photo-template').remove();
+    };
+    $(() => Picture.readJSON());
   };
-
-
-
-  Picture.readJSON = () => {
-    $.get('data/page-1.json', 'json')
-      .then(data => {
-        data.forEach(item => {
-          Picture.allPictures.push(new Picture(item));
-        });
-      }) 
-      .then(Picture.loadPictures);
-  };
-
-  Picture.loadPictures = () => {
-    Picture.allPictures.forEach(pic => pic.render());
-    $('#photo-template').remove();
-  };
-  $(() => Picture.readJSON());
-  
+  _displayImages();
 });
 //copy html templat to JS
